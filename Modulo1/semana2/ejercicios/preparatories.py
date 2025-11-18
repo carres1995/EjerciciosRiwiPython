93acae87f774a5709884006a8ea1964ec5e68299
Carlos <carlosres1995@gmail.com>
2025-11-18 12:32:42 -0500

    feat: exersice books finish

--- Modulo1/semana2/ejercicios/preparatories.py
+++ Modulo1/semana2/ejercicios/preparatories.py
@@ -10,13 +10,13 @@ Prestar libros (cambiar su estado a “prestado”).
 Devolver libros.
 Ver el historial de préstamos."""
 
-def menu():
+def main():
     while True:
-        print("1. Agregar nuevos libros")
-        print("2. Ver libros")
-        print("3. Prestamos libros")
-        print("4. Ver historial de prestamos")
-        print("5. Salir")
+        print("1. Add new books")
+        print("2. View books")
+        print("3. Books loans")
+        print("4. View books loans")
+        print("5. Exit")
         try:
             option=int(input("\nEscoge una opcion: "))
         except ValueError:
@@ -24,16 +24,16 @@ def menu():
             continue
 
         if option == 1:
-            nombre=input("\nIngresa nombre del libro: ")
-            autor=input("Ingresa nombre del autor: ")
-            editor= input("Ingresar editorial ")
+            nombre=input("\nEnter book name: ")
+            autor=input("Enter autor name: ")
+            editor= input("Enter editorial ")
             createBooks(nombre,autor,editor)
         elif option == 2:
             readBooks()
         elif option == 3:
-            pass
+            menuLedingBooks()
         elif option == 4:
-            pass
+            historialBook()
         elif option == 5:
             print("\nThanks you for logging!")
             break
@@ -50,7 +50,7 @@ def createBooks(nombre, autor, editor):
         "name": nombre,
         "author":autor,
         "editor":editor,
-        "state":"disponible"
+        "state":"available"
     })
     return print(f"\nID: {id} | Book: {nombre} | Author | {autor} | Editor: {editor}\n")
 
@@ -58,9 +58,9 @@ def readBooks():
     if not libros:
         print("Empty list")
     else:
-        print(f"\n------------------------------LISTA LIBROS------------------------------------------------")
+        print(f"\n------------------------------BOOKS LIST------------------------------------------------")
         for i in libros:
-            print(f"| ID: {i["id"]:5} | Book: {i["name"]:15} | Author | {i["author"]:15} | Editor: {i["editor"]:15} | State: {i["state"]} |")
+            print(f"| ID: {i["id"]:5} | Book: {i["name"]:15} | Author | {i["author"]:15} | Editor: {i["editor"]:15} | State: {i["state"]:15} |")
         print("------------------------------------------------------------------------------------------\n")  
 historial=[]
 
@@ -68,33 +68,69 @@ def menuLedingBooks():
     while True:
         print("1. Leding Book")
         print("2. Return book")
+        print('3. Exit')
         try:
             option=int(input("\nEscoge una opcion: "))
         except ValueError:
             print("Option not validate")  
             continue
         if option == 1:
-            pass
+            readBooks()
+            num=int(input('Enter id book: '))
+            lendBook(num)
         elif option == 2:
-            pass
+            readBooks()
+            num=int(input('Enter id book: '))
+            returnBook(num)
+        elif option == 3:
+            break
+        else:
+            print('option not validate.\n')   
 
 def historialBook():
-    if not libros:
+    if not historial:
         print("Empty list")
-    else:
-        for l in libros:
-            pass
-
-def lendingBooks(lend, repay):
-    if lend == True:
-        estado="no disponible"
-        return 
-    if repay == False:
-        estado="disponible"
-        return 
- #guardar historial de prestamo de libros ver de forma cambiar y actualizar el readbook()   
+        
+    print(f"\n------------------------------HISTORY OF THE BOOKS------------------------------------------------")    
+    for h in historial:
+        print(f'| ID: {h["id_libro"]:5} | Book: {h["name"]:15} | State | {h["state"]:15} ')
+    print("------------------------------------------------------------------------------------------\n")    
+def lendBook(id:int):
+    if not libros:
+        print('Empty list')
+    for l in libros:    
+        if id == l['id']:
+            if l['state'] == 'borrowed':
+                print("Book is already on loan")
+                return
+            l['state']='borrowed'
+            print(f'Book {l['name']} succesfully borrowed')
+            historial.append({
+                'id_libro':l['id'],
+                'name':l['name'],
+                'state':'borrowed'
+            })   
+            return 
+    print('id not found')    
+        
+def returnBook(id:int):
+    if not libros:
+        print('Empty list')
+    for h in libros:
+        if id == h['id']:
+            if h['state'] == 'available':
+                print('The book is not borrowed')
+            h['state'] = 'available'
+            historial.append({
+                'id_libro':h['id'],
+                'name':h['name'],
+                'state':'available'
+            }) 
+            return
+    print('ID not found')       
+   
     
 
 if __name__=="__main__":
-    menu()
+    main()
 
